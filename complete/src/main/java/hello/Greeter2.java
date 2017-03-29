851d1fac2264094b6b11354d7f2bd99298755f92
Marco Niero <marco.niero86@gmail.com>
2017-03-29 10:04:03 +0200

    nuovo aggiornamento

---
+++ complete/src/main/java/hello/Greeter2.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by marconiero on 29/03/17.
+ */
+public class Greeter2 {
+    public String sayHello2() {
+        return "Hello2" + " " + "world2";
+    }
+}
--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -2,7 +2,10 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         Greeter greeter = new Greeter();
+        Greeter2 greeter2 = new Greeter2();
         System.out.println(greeter.sayHello());
+        System.out.println(greeter2.sayHello2());
     }
 }
